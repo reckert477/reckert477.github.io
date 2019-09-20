bc9e6683a74958965f5209ee860c7084cc8ef9ee
SACCFFT <reck98@gmail.com>
2019-09-20 18:15:27 -0400

    using plain HTML after all

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    path('psite/', include('psite.urls')),
+    path('admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
 ]

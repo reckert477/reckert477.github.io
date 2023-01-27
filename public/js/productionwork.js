dfcf177705095c4b64297c2eb0c11cfbcb15ff51
Ryan Eckert <Ryan@martins-imac.lan>
2023-01-27 07:11:50 -0800

    website almost complete

--- public/js/productionwork.js
+++ public/js/productionwork.js
@@ -21,13 +21,26 @@ productionwork =
 "Technical Production" : {
 
     "offkaiexpo" : {
-        "title" : "OffKai Expo AV Head",
+        "title" : "OffKai Expo AV Lead",
         "subtitle" : "Directed the audiovisual team for OffKai Expo, a convention for virtual youtubers.",
         "cardImage" : "images/offkaiexpo/cover.jpg",
         "about": "Designing and directing audiovisual systems to a hybrid convention for both in person and virtual attendees.",
         "organization" : "OffKai Expo",
-        "role" : "Audiovisual team head",
-        "tools": "Jira, OBS, VDO.ninja, M32 edit, Vectornator, BMD Video Production Suite" 
+        "role" : "Audiovisual Team Lead",
+        "tools": "Jira, OBS, VDO.ninja, M32 edit, Vectornator, BMD Video Production Suite, UML diagrams" 
+    },
+
+    
+
+    "etcfestival" : {
+        "title" : "ETC Festival Livestream Technical Director",
+        "subtitle" : "Designed and directed a livestreaming system for the ETC Festival.",
+        "cardImage" : "images/etcfestival/cover.jpg",
+        "about": "Designing and directing a streaming solution that enabled remote participants to become involved in the ETC winter festival.",
+        "organization" : "CMU Entertainment Technology Center",
+        "role" : "Technical Director",
+        "team" : "12 people",
+        "tools" : "OBS, ElGato Capture Cards, Behringer X32, Wowza RTMP Server, Youtube livestreaming services, UML diagrams"
     },
 
     "abtech" : {
@@ -36,31 +49,21 @@ productionwork =
         "cardImage" : "images/abtech/cover.png",
         "about": "Managed AB Tech, an organization dedicated to supporting students events on campus such as concerts and shows. Personally managed and produced multiple such events.",
         "organization" : "AB Tech",
-        "roles" : "Head of Tech, Event Director, Audio Engineer, Lighting Engineer, Stage Manager",
+        "roles" : "Head of Tech (President), Event Director, Audio Engineer, Lighting Engineer, Stage Manager",
         "organization size" : "30+ people",
-        "tools" : "Vectorworks, Dante, Yamaha QL & CL Series, ArrayCalc, Soundvision, GrandMA2, QLab, Audacity"
-    },
-
-    "etcfestival" : {
-        "title" : "ETC Festival Livestream Technical Director",
-        "subtitle" : "Designed and directed a livestreaming system for the ETC Festival.",
-        "cardImage" : "images/etcfestival/cover.jpg",
-        "about": "Designing and directing a streaming solution that enabled remote participants to become involved in the ETC winter festival.",
-        "organization" : "OffKai Expo",
-        "role" : "Lead technician",
-        "team" : "12 people",
-        "tools" : "OBS, ElGato Capture Cards, Behringer X32, FFMPEG RTMP Server, Youtube livestreaming services"
+        "tools" : "Vectorworks, Dante, Yamaha QL & CL Series, ArrayCalc, Soundvision, GrandMA2, QLab, GSuite"
     },
 
     "scottycon" : {
         "title" : "ScottyCon",
-        "subtitle" : "Co-founder of an anime and gaming convention aimed towards CMU students. Winner of SLICE 2019 best new initiative.",
+        "subtitle" : "Founded an anime and gaming convention aimed towards CMU students. Winner of SLICE 2019 most outstanding new initiative.",
         "cardImage" : "images/scottycon/cover.png",
-        "about": "Designing and directing a streaming solution that enabled remote participants to become involved in the ETC winter festival.",
+        "about": "Founded an anime and gaming convention aimed towards CMU students. Winner of SLICE 2019 most outstanding new initiative, with over 500 attendees on its first year running.",
         "organization" : "Conegie Mellon",
         "role" : "Co-founder, Director",
         "team" : "15 people",
-        "tools" : "Figma"
+        "tools" : "Figma, GSuite",
+        "award" : "2019 Most Outstanding New Initiative - Office of Student Leadership, Involvement, and Civic Engagement"
     }
 
 }
